71a56fab067466ae1cc1c444f666b38ed0ec75da
snolte26 <snolte26@gmail.com>
2021-09-06 13:05:07 -0400

    Setup for the music path. Everything seems to work fine, although it says that "something went wrong when setting variable to the environment" regardless of if it was successful or not

--- setup.py
+++ setup.py
@@ -7,17 +7,23 @@ def init_env():
 
     load_dotenv()
 
-    if os.getenv('OWM_KEY'):
-        #print('\nsetup_env function ran, but Open Weather Map key is already set!')
-        #print('OWM_KEY is: ' + os.getenv('OWM_KEY'))
+    if os.getenv('OWM_KEY') and os.getenv('MUSIC_PATH'):
+        # print('\nsetup_env function ran, but Open Weather Map key is already set!')
+        # print('OWM_KEY is: ' + os.getenv('OWM_KEY'))
         return
-
+    '''
+    if os.getenv('MUSIC_PATH'):
+        # print('\nsetup_env function ran, but Open Weather Map key is already set!')
+        # print('MUSIC_PATH is: ' + os.getenv('MUSIC_PATH'))
+        return
+    '''
     # If there is no .env file, create it
     if not os.path.exists('./.env'):
         env_file = open('.env', 'w')
         env_file.close()
 
     set_new_weather_key()
+    set_new_music_path()
 
 
 def set_new_weather_key():
@@ -45,3 +51,37 @@ def set_new_weather_key():
         print('Key has been set to environment!')
     else:
         print('Something went wrong setting OWM key to the environment.')
+
+
+def set_new_music_path():
+    music_path = input('\nEnter or paste music directory here here: ')
+    musicPathList = []
+
+    # Recursion here, in case the user doesn't enter any value
+    if not music_path:
+        set_new_music_path()
+
+    print('Setting Music Directory...')
+
+    for word in music_path.split("\\"):
+        musicPathList.append(word)
+        musicPathList.append("\\" + "\\")
+    musicPathList.pop()
+    music_path = "".join(musicPathList)
+
+    # If the file exists and is not empty, append onto a new line
+    if os.stat('./.env').st_size == 0:
+        print('.env is empty!')
+        env_file = open('.env', 'w')
+        env_file.write(f'MUSIC_PATH={music_path}')
+    else:
+        print('.env is not empty!')
+        env_file = open('.env', 'a')
+        env_file.write(f'\nMUSIC_PATH={music_path}')
+
+    env_file.close()
+
+    if os.getenv('MUSIC_PATH'):
+        print('Path has been set to environment!')
+    else:
+        print('Something went wrong setting Music Path to the environment.')
